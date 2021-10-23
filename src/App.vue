fd9a87ee20f01a8672567e1fb26c1b7557d9ad78
lww9999 <815951925@qq.com>
2021-10-23 19:05:31 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <div>this is my project</div>
+    <!-- <div class="toutiao icon-shipin"></div> -->
     <!-- 路由的出口 -->
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import './styles/index.less'
+// 加载vant组件库   全局导入
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
